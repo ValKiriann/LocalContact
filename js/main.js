c3f16cfbf071bd2a29d5424bd5def540029236a6
ValKiriann <hello@anaenriquez.es>
2018-04-06 19:01:51 +0200

    refactor and stating to store values

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-// JAVASCRIPT PARA ACORDEON
-var acc = document.getElementsByClassName("accordion");
-var i;
+// JAVASCRIPT PARA ACORDEON 
+function accordion() {
+	var acc = document.getElementsByClassName("accordion");
 
-for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var panel = this.nextElementSibling;
-		if (panel.style.maxHeight){
-			panel.style.maxHeight = null;
-		} else {
-			panel.style.maxHeight = panel.scrollHeight + "px";
-		} 
-	});
+	for (var i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight){
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			} 
+		});
+	}
 }
+accordion();
 
+/* -------------------------- NEW FILE ---------------------------------- */
 
 // JAVASCRIPT PARA DISPLAY DE LIBRETA
 var contactBook = document.getElementById('empty-msg');
@@ -24,36 +27,95 @@ if(localStorage.length < 1){
 	/* ADD MAGIC HERE */ 
 }
 
+
+
 // JAVASCRIPT PARA FORMULARIO
+var form = document.addContactForm;
+var phoneInput = form.elements.phone;
+var nameInput = form.elements.firstName;
+var phone = phoneInput.value;
+var name = nameInput.value;
+var phoneError = document.getElementById('phoneError');
+var nameError = document.getElementById('nameError');
+
 function isEmpty(str) {
 	return (!str || 0 === str.length);
 }
+function removeError(input,textError){
+	textError.innerHTML = "";
+	textError.classList.remove('visible');
+	input.classList.remove('wrong');
+}
+function showError(input,textError,errorMessage){
+	textError.innerHTML = errorMessage;
+	textError.classList.add('visible');
+	input.classList.add('wrong');
+}
+
+// EL ERROR CON ESO ME VALE 
+
+function formQualityControl(){
+
+	nameInput.addEventListener("focus", event=> {
+		removeError(nameInput,nameError);
+	});
+	nameInput.addEventListener("focusout", event=> {
+		name = nameInput.value;
+		if(isEmpty(name)){
+			showError(nameInput,nameError,"You must enter a name");
+		} else{
+			removeError(nameInput,nameError);
+		}
+	})
+
+	phoneInput.addEventListener("focus", event => {
+		removeError(phoneInput,phoneError);
+	});
+
+	phoneInput.addEventListener("focusout", event => {
+		phone = phoneInput.value;
+		if(isEmpty(phone)){
+			showError(phoneInput,phoneError,"You must enter a phone");
+		} else{
+			removeError(phoneInput,phoneError);
+		}
+	})
+
+	form.addEventListener("submit", event => {
+		event.preventDefault();
+		var errors = false;
+		
+		if(isEmpty(phone)){
+			showError(phoneInput,phoneError,"You must enter a phone");
+			errors = true;
+		} else{
+			removeError(phoneInput,phoneError);
+		}
+		if(isEmpty(name)){
+			showError(nameInput,nameError,"You must enter a name");
+			errors = true;
+		} else{
+			removeError(nameInput,nameError);
+		}
+		if(!errors){
+			if(addNewContact(name,phone)){
+				showNewContact(name);
+				nameInput.value = "";
+				phoneInput.value = "";
+				// INSERTAR MENSAJE DE VERIFICACION DE CONTACTO
 
 
-var form = document.addContactForm;
-form.addEventListener("submit", event => {
-	event.preventDefault();
-	var phone = form.elements.phone.value;
-	var name = form.elements.firstName.value;
-	var errorPhone = document.getElementById('errorPhone');
-	var errorName = document.getElementById('errorName');
-	if(isEmpty(phone)){
-		errorPhone.innerHTML = "You must enter a phone";
-		errorPhone.classList.add('visible');
-	} else{
-		errorPhone.innerHTML = "";
-		errorPhone.classList.remove('visible');
-	}
-	if(isEmpty(name)){
-		errorName.innerHTML = "You must enter a name";
-		errorName.classList.add('visible');
-		//refactorizar esto a poner dos clases, una xa los span y otra xa cambiar la ralla de los campos
-	} else{
-		errorName.innerHTML = "";
-		errorName.classList.remove('visible');
-	}
+			}
+
+		}
+	});
+}
+formQualityControl();
 
 function addNewContact(name,phone){
-
+    localStorage.setItem(name, phone);
 };
-});
+function showNewContact(key){
+	
+	localStorage.getItem(key);
+}
